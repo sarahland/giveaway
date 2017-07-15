e0371f18eb364b7efe49bf11f0cee681838a5476
Sarah Amirie <asamirie@gmail.com>
2017-07-15 14:16:16 -0400

    fixed mixing of express and React Router routes

---
+++ app/config/routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Router, Route, hashHistory} from 'react-router'
+import Home from '../components/ui/Home'
+import About from '../components/ui/About'
+import DonateNow from '../components/ui/DonateNow'
+
+import ItemList from '../components/ui/ItemList'
+import {Left, Right, Whoops404} from '../components'
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="/" component={Left}>
+      <Route path="about" component={About}/>
+      <Route path="donatenow" component={DonateNow}/>
+      <Route path="item" component={ItemList}/>
+    </Route>
+    <Route path="*" component={Whoops404}/>
+  </Router>
+);
+export default routes;
