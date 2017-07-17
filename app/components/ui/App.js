64e0f44827310d5f9efc9a6031cce395ec167cc4
Moh <mohakemal9@gmail.com>
2017-07-17 06:10:29 -0400

    firebase updated

--- app/components/ui/App.js
+++ app/components/ui/App.js
@@ -15,30 +15,4 @@ export default class App extends Component {
 
 )};
 }
-  render() {
-     return (
-       <View style={styles.container}>
-         <Login />
-       </View>
-     );
-   }
- }
 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center',
-     backgroundColor: '#F5FCFF',
-   },
-   welcome: {
-     fontSize: 20,
-     textAlign: 'center',
-     margin: 10,
-   },
-   instructions: {
-     textAlign: 'center',
-     color: '#333333',
-     marginBottom: 5,
-   },
- });
