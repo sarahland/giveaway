004ba56b9349a73e49053d8380c9ceb63b8f32be
Tsigie Kumssa <tsigiekumssa@Tsigies-MacBook-Pro.local>
2017-07-20 19:26:21 -0400

    updated files

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,9 +1,71 @@
 
 
-var React = require("react");
-var Login = ReactDOM.createClass({
-  render: function() {
+import React, {Component} from 'react';
+import SearchBar from '../containers/search_bar';
+import OrgList from '../containers/orgs_list';
+import {Field, reduxForm } from 'redux-form';
+
+class SignIn extends Component {
+  renderField(field){
+
     return(
 
-});
-module.exports = login;
+        <div className='input-field col s6'>
+          <i className="material-icons prefix">{field.icon}</i>
+          <label htmlFor={field.htmlFor}>{field.label}</label>
+          <input
+            className = {field.className}
+            type={field.type}
+            {...field.input}
+          />
+        </div>
+    )
+  }
+
+  render() {
+    return (
+      <div id="loginpg" className=" center logincontainer animated bounceInDown">
+        <div className="row">
+          <form id="loginform" className="col m12">
+            {/* TODO: only hover on underline activates form */}
+            <Field
+              label = "Username"
+              name = "username"
+              icon = 'person'
+              // type = 'text'
+              htmlFor="icon_prefix2"
+              className = 'validate'
+              component={this.renderField}
+            />
+            <Field
+              label = "Password"
+              name = "password"
+              type = "password"
+              htmlFor="password"
+              icon = 'lock outline'
+              component={this.renderField}
+            />
+            <div className="row center">
+              <div className="col m6">
+                <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
+                  <i className="material-icons right">send</i>
+                </button>
+              </div>
+              <div className="col m6">
+                <button id="resetbutton" className="btn waves-effect waves-light" onclick="resetFunc" value="Reset Form" name="action">Reset
+                  <i className="material-icons right">send</i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+
+export default reduxForm({
+  form: 'PostNewForm'
+})(SignIn);
--- app/components/SignUp.js
+++ app/components/SignUp.js
@@ -1,8 +1,148 @@
 
-var React = require("react");
-var SignUp = ReactDOM.createClass({
-  render: function() {
-    return(
 
-});
-module.exports = SignUp;
+import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
+import OrgList from '../containers/item_list';
+import {Field, reduxForm } from 'redux-form';
+
+class SignUp extends Component {
+  renderField(field){
+    return(
+        <div className={field.divClassName}>
+          <input
+            className = {field.inputClassName}
+            type = {field.type}
+            placeholder = {field.placeholder}
+            {...field.input}
+          />
+          <label htmlFor={field.htmlFor}>{field.label}</label>
+        </div>
+    )
+  }
+
+  render() {
+    return (
+        <div className="container">
+
+        <div id="signuppage" className="signupcontainer animated bounceInRight">
+          <div className="row">
+            <form id="signupform" className="col m12">
+              <div className="row">
+                <Field
+                  name='FirstName'
+                  id="first_name"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m6"
+                  htmlFor="first_name"
+                  label="First Name"
+                  component={this.renderField}
+                />
+                <Field
+                  name='LastName'
+                  id="last_name"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m6"
+                  htmlFor="last_name"
+                  label="Last Name"
+                  component={this.renderField}
+                />
+              </div>
+              <div className="row">
+                <Field
+                  name='Address'
+                  id="address"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m12"
+                  htmlFor="address"
+                  label="Adress"
+                  component={this.renderField}
+                />
+              </div>
+              <div className="row">
+                <Field
+                  name='City'
+                  id="city"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m4"
+                  htmlFor="city"
+                  label="City"
+                  component={this.renderField}
+                />
+                <Field
+                  name='State'
+                  id="state"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m4"
+                  htmlFor="state"
+                  label="state"
+                  component={this.renderField}
+                />
+                <div className="row">
+                  <Field
+                    name='zipCode'
+                    id="zip_code"
+                    // type="text"
+                    inputClassName="validate"
+                    divClassName="input-field col m4"
+                    htmlFor="zip_code"
+                    label="Zip Code"
+                    component={this.renderField}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <Field
+                  name='signupPassword'
+                  id="sign_up_password"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m12"
+                  htmlFor="sign_up_password"
+                  label="Signup Password"
+                  component={this.renderField}
+                />
+              </div>
+              <div className="row">
+                {/* <div className="input-field col m12">
+                  <input id="confirmpassword" type="password" className="validate" />
+                  <label htmlFor="confirmpassword">Confirm Password</label>
+                </div> */}
+                <Field
+                  name='confirmPassword'
+                  id="confirm_password"
+                  // type="text"
+                  inputClassName="validate"
+                  divClassName="input-field col m12"
+                  htmlFor="confirm_password"
+                  label="Confirm Password"
+                  component={this.renderField}
+                />
+              </div>
+              <div className="row center">
+                <div className="col m6">
+                  <button id="resetbutton" className="btn waves-effect waves-light" onclick="resetFunc" value="Reset Form" name="action">Reset
+              <i className="material-icons right">send</i>
+              </button>
+                </div>
+                <div className="col m6">
+                  <button id="loginsubmit" className="btn waves-effect waves-light" type="submit" name="action">Submit
+              <i className="material-icons right">send</i>
+              </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostNewUser'
+})(SignUp);
