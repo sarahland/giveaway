3ad28174041f601e91471828e49d0e68428d2269
Moh Kemal <mohakemal9@gmail.com>
2017-07-20 09:00:55 -0400

    updated

--- app/components/ui/ItemList.js
+++ app/components/ui/ItemList.js
@@ -14,28 +14,84 @@ class ItemList extends Component {
           donatedBy: "Mohammed",
           email: "mohakemal9@gmail.com",
           thumbnail: "../assets/Samsung.jpg"
-        }, {
-          item: "Shose",
+        },
+        {
+          item: "Acer Laptop",
           donatedBy: "Mohammed",
           email: "mohakemal9@gmail.com",
-          thumbnail: "../assets/shose2.jpg"
-        }, {
-          item: "Shose",
+          thumbnail: "../assets/acer.jpg"
+        },
+        {
+          item: "Computer Cord",
+          donatedBy: "Mohammed",
+          email: "mohfun@gmail.com",
+          thumbnail: "../assets/cord.jpg"
+        },
+        {
+          item: "Laptop Cord",
+          donatedBy: "Mohammed",
+          email: "m.reshid@gmail.com",
+          thumbnail: "../assets/cord2.jpg"
+        },
+        {
+          item: "Moderately Used HP",
+          donatedBy: "Mohammed",
+          email: "mohemal9@gmail.com",
+          thumbnail: "../assets/hp2.jpg"
+        },
+        {
+          item: "Ipad",
+          donatedBy: "Mohammed",
+          email: "sumara@gmail.com",
+          thumbnail: "../assets/ipad.jpg"
+        },
+        {
+          item: "Android Phone",
+          donatedBy: "Mohammed",
+          email: "faris@gmail.com",
+          thumbnail: "../assets/phone.jpg"
+        },
+        {
+          item: "HP Laptop",
+          donatedBy: "Mohammed",
+          email: "anyone@gmail.com",
+          thumbnail: "../assets/laptop.jpg"
+        },
+        {
+          item: "Tape Recorder",
+          donatedBy: "Mohammed",
           email: "mohakemal9@gmail.com",
-          thumbnail: "../assets/shose.jpg"
-        }, {
-          item: "Sofa",
-          email: "mohakemal9@gmail.com",
-          thumbnail: "../assets/sofa.jpg"
-        }, {
-          item: "Book",
-          email: "mohakemal9@gmail.com",
-          thumbnail: "../assets/book.jpg"
-        }, {
+          thumbnail: "../assets/tape.jpg"
+        },
+        {
           item: "Samsung TV",
           email: "mohakemal9@gmail.com",
           thumbnail: "../assets/samsung.jpg"
+        },
+
+        {
+          item: "Shoes",
+          donatedBy: "Mohammed",
+          email: "mohakemal9@gmail.com",
+          thumbnail: "../assets/shose2.jpg"
+        },
+        {
+          item: "Shoese",
+          email: "mohakemal9@gmail.com",
+          thumbnail: "../assets/shose.jpg"
+        },
+        {
+          item: "Sofa",
+          email: "mohakemal9@gmail.com",
+          thumbnail: "../assets/sofa.jpg"
+        },
+        {
+          item: "Book",
+          email: "mohakemal9@gmail.com",
+          thumbnail: "../assets/book.jpg"
         }
+
+
       ]
     }
   }
